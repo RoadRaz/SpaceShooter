ae1d2bf6ba677424e42fbe4e718ed5baa02acad6
Soahm <sk4732@rit.edu>
2020-04-07 09:44:30 -0400

    Working Health Bar
    Scene : AbhiSpawning

--- SpaceShooter/Assets/Script/HealthBarScript.cs
+++ SpaceShooter/Assets/Script/HealthBarScript.cs
@@ -4,21 +4,25 @@ using UnityEngine;
 using UnityEngine.UI;
 public class HealthBarScript : MonoBehaviour
 {
+    
     Image healthBar;
     float maxHealth = 100f;
     public static float health;
+    public GameObject Player;
 
     // Start is called before the first frame update
     void Start()
     {
         healthBar = GetComponent<Image>();
         health = maxHealth;
+        //GetComponent<PlayerControl>().health
     }
 
     // Update is called once per frame
     void Update()
     {
         //health = GetComponent<PlayerControl>().health;
+        health = Player.gameObject.GetComponent<PlayerControl>().health;
         healthBar.fillAmount = health / maxHealth; 
     }
 }
